cf1cd49f7661637278328d604c1f42c314fd6c73
Rimsha Arshad <adeelarshad8900@gmail.com>
2024-03-27 18:50:59 -0700

    gamming project

---
+++ index.js
@@ -0,0 +1,20 @@
+#! /urs/bin/env node
+import inquirer from "inquirer";
+// 1) computer will generate a random number
+// 2) user input for guessing number
+// 3) compare user input with computer generater number and show result
+const randomNumber = Math.floor(Math.random() * 7 + 1);
+const number = await inquirer.prompt([
+    {
+        name: "userGuessNumber",
+        type: "number",
+        message: "guess number between 1-7"
+    }
+]);
+if (number.userGuessNumber === randomNumber) {
+    console.log("congruntulation you guess right number");
+}
+else {
+    console.log("you guess wrong number");
+}
+;
